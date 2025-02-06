ddce220329905340d66cf1319b5a3d3199f2e1c9
Mark <markc9309@yahoo.com>
2025-02-06 07:45:30 +0000

    added styling and pagination to posts

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
-    # queryset = Post.objects.filter(status=1)
-    # template_name = "list_post.html"
+    # model = Post
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
